b0318b9de9460e35e1b0890523464be618a272ab
Roman <bekrenov.roman@gmail.com>
2024-04-22 18:37:47 +0200

    Initial commit

---
+++ src/main/java/com/bekrenovr/spotkajmysie/SpotkajmySieApplication.java
@@ -0,0 +1,13 @@
+package com.bekrenovr.spotkajmysie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotkajmySieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotkajmySieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bekrenovr/spotkajmysie/SpotkajmySieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekrenovr.spotkajmysie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotkajmySieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
